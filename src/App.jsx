56803133518549834756b32e1bfc08a6493eed0f
MissCeptstudios <joanna.cholas@gmail.com>
2025-06-11 13:09:59 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import IBCalculator from './IBCalculator.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function App() {
+  return (
+    <div className="App">
+      <IBCalculator />
+    </div>
+  )
+}
+
+export default App
